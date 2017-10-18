b4021d2b54a552c4d195f81334531800b1cddac2
ManiacTwister <ManiacTwister@users.noreply.github.com>
2017-10-18 13:36:05 +0200

    Update ffprobe.py

--- ffprobe/ffprobe.py
+++ ffprobe/ffprobe.py
@@ -27,10 +27,11 @@ class FFProbe:
 		except:
 			raise IOError('ffprobe not found.')			
 		if os.path.isfile(video_file):
+			# Doesn't work on linux based systems if we don't quote the path
 			if str(platform.system())=='Windows':
 				cmd=["ffprobe","-show_streams",self.video_file]
 			else:
-				cmd=["ffprobe -show_streams "+pipes.quote(self.video_file)]
+				cmd=["ffprobe -show_streams " + pipes.quote(self.video_file)]
 
 			p = subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
 			self.format=None
